1dd16616ddd7ef78094825e4fe0da622a31472c3
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-08-12 20:56:10 -0700

    Succeeded at challenge

---
+++ 401-javascript/arrayBinarySearch/array-binary-search.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const binarySearch = (arr, value) => {
+  let middleIndex = Math.floor((arr.length - 1)/2);
+  let firstIndex = 0;
+  let lastIndex = arr.length - 1;
+
+  while(arr[middleIndex] !== value) {
+    if (arr[middleIndex] > value) {
+      lastIndex = middleIndex - 1;
+      middleIndex = Math.floor((lastIndex - firstIndex)/2) + firstIndex;
+    }
+    else if (arr[middleIndex] < value) {
+      firstIndex = middleIndex + 1;
+      middleIndex = Math.floor((lastIndex - firstIndex)/2) + firstIndex;
+    }
+    if (firstIndex > lastIndex) {
+      return -1;
+    }
+  }
+  return middleIndex;
+};
+
+module.exports = binarySearch;
