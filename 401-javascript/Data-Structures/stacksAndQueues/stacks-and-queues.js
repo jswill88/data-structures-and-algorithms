f10c14efeb42f554e1ad0a711aebf3751965bd4d
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-08-21 17:39:08 -0700

    Complete stacks and Queues

--- 401-javascript/Data-Structures/stacksAndQueues/stacks-and-queues.js
+++ 401-javascript/Data-Structures/stacksAndQueues/stacks-and-queues.js
@@ -27,6 +27,37 @@ class Stack {
   }
 }
 
+class Queue {
+  constructor(){
+    this.front = null;
+  }
+  enqueue(value) {
+    if(this.isEmpty()) {
+      this.front = new Node(value);
+      this.rear = this.front;
+    } else {
+      this.rear.next = new Node(value)
+      this.rear = this.rear.next;
+    }
+  }
+  dequeue() {
+    if(this.isEmpty()) {
+      throw new RangeError('Cannot dequeue an empty queue');
+    }
+    let temp = this.front;
+    this.front = this.front.next;
+    temp.next = null;
+    return temp.value;
+  }
+  peek() {
+    if (this.front) { return this.front.value; }
+    throw new RangeError('Cannot peek an empty queue');
+  }
+  isEmpty() {
+    return !this.front;
+  }
+}
+
 class Node {
   constructor(value, next = null) {
     this.value = value;
@@ -34,4 +65,4 @@ class Node {
   }
 }
 
-module.exports = Stack;
+module.exports = { stack: Stack, queue: Queue };
--- 401-javascript/Data-Structures/stacksAndQueues/stacks-and-queues.test.js
+++ 401-javascript/Data-Structures/stacksAndQueues/stacks-and-queues.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const Stack = require('./stacks-and-queues');
+const Stack = require('./stacks-and-queues').stack;
+const Queue = require('./stacks-and-queues').queue;
 
 describe('Stack Tests', () => {
   it('Can instatiate an empty stack', () => {
@@ -47,6 +48,55 @@ describe('Stack Tests', () => {
     expect(()=>stack.peek()).toThrowError('Cannot peek an empty stack');
   })
 });
+
+describe('Queue Tests', () => {
+  it('Can instantiate an empty Queue', () => {
+    const queue = new Queue();
+    expect(queue.isEmpty()).toBeTruthy();
+  })
+  it('Can enqueue a new value', () => {
+    const queue = new Queue();
+    queue.enqueue('Paranoid Android');
+    expect(queue.isEmpty()).toBeFalsy();
+  })
+  it('Can peek a value', () => {
+    const queue = new Queue();
+    queue.enqueue('Paranoid Android');
+    expect(queue.peek()).toBe('Paranoid Android');
+  })
+
+  it('Can enqueue many values', () => {
+    const queue = bigQueue();
+    expect(queue.peek()).toBe('Paranoid Android');
+  })
+
+  it('Can throw an error when peeking an empty queue', () => {
+    const queue = new Queue();
+    expect(()=> queue.peek()).toThrowError('Cannot peek an empty queue');
+  })
+  it('Can dequeue the front value', () => {
+    const queue = new Queue();
+    queue.enqueue('Paranoid Android');
+    queue.dequeue();
+    expect(queue.isEmpty()).toBeTruthy();
+  })
+  it('Can dequeue the correct value', () => {
+    const queue = bigQueue();
+    let blizzard = queue.dequeue();
+    expect(blizzard).toBe('Paranoid Android')
+  })
+  it('Can throw an error when dequeueing an empty queue', () => {
+    const queue = new Queue();
+    expect(() => queue.dequeue()).toThrowError('Cannot dequeue an empty queue')
+  })
+  it('Can empty a queue after several dequeues', () => {
+    const queue = bigQueue();
+    for(let i = 0; i < 4; i++) { queue.dequeue(); }
+    expect(queue.isEmpty()).toBeTruthy();
+  })
+
+})
+
 const bigStack = () => {
   const stack = new Stack();
   stack.push('Fun fun fun');
@@ -54,4 +104,12 @@ const bigStack = () => {
   stack.push('God Only Knows');
   stack.push('Good Vibrations');
   return stack;
+};
+const bigQueue = () => {
+  const queue = new Queue();
+  queue.enqueue('Paranoid Android');
+  queue.enqueue('15 Step');
+  queue.enqueue('Videotape');
+  queue.enqueue('Creep');
+  return queue;
 }
