c3101d6426434a661c9b581b75aa5377e0b9dc5e
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-10-03 18:13:36 -0700

    fix bug

--- 401-javascript/whiteboardPractice/ll-dupes.test.js
+++ 401-javascript/whiteboardPractice/ll-dupes.test.js
@@ -4,8 +4,6 @@ const llDupes = require('./ll-dupes');
 const LinkedList = require('../Data-Structures/linkedList/linked-list')
 
 const linkedList = new LinkedList();
-const D = linkedList.insert('D')
-const B = linkedList.insert('B');
 linkedList.insert('F');
 linkedList.insert('D');
 linkedList.insert('B');
@@ -13,7 +11,6 @@ linkedList.insert('A');
 
 
 describe('ll dupes tests', () => {
-  // const ll = makeList();
   it('should ', () => {
 
     expect(llDupes(linkedList)).toEqual(['B', 'D'])
