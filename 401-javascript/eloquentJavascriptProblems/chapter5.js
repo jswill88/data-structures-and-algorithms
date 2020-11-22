b6cac68e68d51faaf5eb05afbb2f87073591fa27
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-11-22 15:00:04 -0800

    more work on chapter 5

--- 401-javascript/eloquentJavascriptProblems/chapter5.js
+++ 401-javascript/eloquentJavascriptProblems/chapter5.js
@@ -6,6 +6,29 @@ const flatten = arr => {
   }, []);
 
 };
-const twoLayerArray = [[2, 3, 4, [1, 2,[ 'x', 'y', 'z']]], ['n', 'd', 's', 'k', 'j'], ['s', 'd', 'l', 'f', 'j']];
+const twoLayerArray = [[2, 3, 4, [1, 2, ['x', 'y', 'z']]], ['n', 'd', 's', 'k', 'j'], ['s', 'd', 'l', 'f', 'j']];
 console.log(flatten(twoLayerArray));
 
+
+const loop = (value, testFunction, updateFunction, bodyFunction) => {
+  if(!testFunction(value)) return;
+  bodyFunction(value);
+  const newValue = updateFunction(value);
+  loop(newValue, testFunction, updateFunction, bodyFunction);
+};
+
+loop( 0, x => x < 10, y => y + 2, i => {
+  console.log(i * 5)
+})
+
+const every = (arr, predicateFunction) => {
+  // let isValid = true;
+  // for( let val of arr) {
+  //   if (!predicateFunction(val)) {
+  //     isValid = false;
+  //   }
+  // }
+  // return isValid;
+  return !arr.some(val => !predicateFunction(val))
+}
+console.log('every', every([1,2,3], x => x > 0))
