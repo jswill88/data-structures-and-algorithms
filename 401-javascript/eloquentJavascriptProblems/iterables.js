4148225f37b25b902dc1a3355182e45a7d5b5621
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-11-27 11:58:34 -0800

    iterables practice

---
+++ 401-javascript/eloquentJavascriptProblems/iterables.js
@@ -0,0 +1,60 @@
+class Matrix {
+  constructor(width, height, element = () => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x, y);
+      }
+    }
+  }
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+  [Symbol.iterator]() {
+    return new MatrixIterator(this);
+  }
+}
+
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+  next() {
+    if (this.y >= this.matrix.height) {
+      return { done: true };
+    }
+    let value = {
+      x: this.x,
+      y: this.y,
+      value: this.matrix.get(this.x, this.y)
+    };
+    this.x++;
+    if (this.x >= this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+    return {value, done: false}
+  }
+}
+
+let matrix = new Matrix(2, 2, (x,y) => `value ${x},${y}`);
+for (let {x, y, value} of matrix) {
+  console.log(x, y, value)
+}
+
+let varyingSize = {
+  get size() {
+    return Math.floor(Math.random() * 100)
+  }
+}
+console.log(varyingSize.size);
+
