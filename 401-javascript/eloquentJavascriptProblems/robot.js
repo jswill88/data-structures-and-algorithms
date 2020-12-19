95a8d4253a94104f0c7941807028e12f88309be9
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-12-19 00:36:59 -0800

    random robot exercise complete

--- 401-javascript/eloquentJavascriptProblems/robot.js
+++ 401-javascript/eloquentJavascriptProblems/robot.js
@@ -48,6 +48,18 @@ class VillageState {
       return new VillageState(destination, parcels)
     }
   }
+  static random(parcelCount = 5) {
+    let parcels = [];
+    for(let i = 0; i < parcelCount; i++) {
+      let address = randomPick(Object.keys(roadGraph));
+      let place;
+      do {
+        place = randomPick(Object.keys(roadGraph));
+      } while (place === address);
+      parcels.push({ place, address});
+    }
+    return new VillageState('Post Office', parcels)
+  }
 }
 let first = new VillageState('Post Office',[{place: 'Post Office', address: 'Alice\'s House'}]);
 
@@ -55,3 +67,26 @@ let next = first.move('Alice\'s House');
 console.log(next.parcels);
 console.log(first.place);
 
+function runRobot(state, robot, memory) {
+  for(let turn = 0;; turn++) {
+    if(state.parcels.length === 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state,memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+function randomRobot(state) {
+  return {
+    direction: randomPick(roadGraph[state.place])
+  };
+}
+runRobot(VillageState.random(), randomRobot);
