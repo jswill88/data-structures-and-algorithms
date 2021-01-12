1ef8459da2a40973ffb9e9f07733fd1b9e2cef77
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-01-12 15:37:24 -0800

    find all dupes grok challenge complete

--- 401-javascript/grokkingTheCodingInterview/findDupe.js
+++ 401-javascript/grokkingTheCodingInterview/findDupe.js
@@ -1,4 +1,4 @@
-module.exports = function(nums) {
+const findDupe = function(nums) {
   let i = 0;
   while(i < nums.length) {
     let j = nums[i] - 1;
@@ -8,3 +8,20 @@ module.exports = function(nums) {
   }
   return -1;
 };
+
+const find_all_duplicates = function(nums) {
+  const duplicateNumbers = [];
+  let i = 0;
+  while (i < nums.length) {
+    let j = nums[i] - 1;
+    if (nums[i] === nums[j]) i++
+    else [nums[i], nums[j]] = [nums[j], nums[i]];
+  }
+  for(i = 0; i < nums.length; i++) {
+    if(nums[i] - 1 !== i) duplicateNumbers.push(nums[i]);
+  }
+  return duplicateNumbers;
+};
+
+module.exports = { findDupe, find_all_duplicates };
+
