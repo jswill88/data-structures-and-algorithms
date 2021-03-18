55c08d4efe885d4ab672b2321529a908e0556d75
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-03-17 21:09:15 -0700

    draw background of platform game

--- 401-javascript/eloquentJavascriptProblems/PlatformGame/app.js
+++ 401-javascript/eloquentJavascriptProblems/PlatformGame/app.js
@@ -29,7 +29,7 @@ class State {
 }
 
 class Vec {
-  constructor (x,y) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
   }
@@ -50,7 +50,7 @@ class Player {
   get type() { return 'player'; }
 
   static create(pos) {
-    return new Player(pos.plus(new Vec(0, -.5)), new Vec(0,0))
+    return new Player(pos.plus(new Vec(0, -.5)), new Vec(0, 0))
   }
 }
 
@@ -66,17 +66,17 @@ class Lava {
   get type() { return 'lava'; }
 
   static create(pos, ch) {
-    if(ch === '=') {
-      return new Lava(pos, new Vec(2,0));
+    if (ch === '=') {
+      return new Lava(pos, new Vec(2, 0));
     } else if (ch === '|') {
-      return new Lava(pos, new Vec(0,2));
+      return new Lava(pos, new Vec(0, 2));
     } else if (ch === 'v') {
-      return new Lava(pos, new Vec(0,3), pos);
+      return new Lava(pos, new Vec(0, 3), pos);
     }
   }
 }
 
-Lava.prototype.size = new Vec(1,1);
+Lava.prototype.size = new Vec(1, 1);
 
 class Coin {
   constructor(pos, basePos, wobble) {
@@ -114,16 +114,94 @@ class Level {
     this.startActors = [];
 
     this.rows = rows.map((row, y) => {
-      return row.map((ch,x) => {
+      return row.map((ch, x) => {
         let type = levelChars[ch];
         if (typeof type === 'string') return type;
         this.startActors.push(
-          type.create(new Vec(x,y), ch));
+          type.create(new Vec(x, y), ch));
         return 'empty';
       });
     });
   }
 }
 
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attrs[attr]);
+  }
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+class DOMDisplay {
+  constructor(parent, level) {
+    this.dom = elt('div', { class: 'game' }, drawGrid(level));
+    this.actorLayer = null;
+    parent.appendChild(this.dom);
+  }
+  clear() { this.dom.remove(); }
+}
+
+const scale = 20;
+
+function drawGrid(level) {
+  return elt('table', {
+    class: 'background',
+    style: `width: ${level.width * scale}px`
+  }, ...level.rows.map(row =>
+    elt('tr', { style: `height: ${scale}px` },
+      ...row.map(type => {
+        console.log('type', type)
+        return elt('td', { class: type })
+      }))
+  ));
+}
+
+function drawActors(actors) {
+  return elt('div', {}, ...actors.map(actor => {
+    let rect = elt('div', {class: `actor ${actor.type}`});
+    rect.style.width = `${actor.size.x * scale}px`;
+    rect.style.height = `${actor.size.y * scale}px`;
+    rect.style.left = `${actor.pos.x * scale}px`;
+    rect.style.top = `${actor.pos.y * scale}px`;
+    return rect;
+  }));
+}
+
+DOMDisplay.prototype.syncState = function (state) {
+  if(this.actorLayer) this.actorLayer.remove();
+  console.log(state.actors)
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.status}`;
+  this.scrollPlayerIntoView(state);
+}
+DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
+  let width = this.dom.clientWidth;
+  let height = this.dom.clientHeight;
+  let margin = width/3;
+
+  // The viewport
+  let left = this.dom.scrollLeft;
+  let right = left + width;
+  let top = this.dom.scrollTop;
+  let bottom = top + height;
+
+  let player = state.player;
+  let center = player.pos
+    .plus(player.size.times(.5)).times(scale);
+
+  if(center.x < left + margin) {
+    this.dom.scrollLeft = center.x - margin;
+  } else if (center.x > right - margin) {
+    this.dom.scrollTop = center.y + margin - height;
+  }
+};
+
 let simpleLevel = new Level(simpleLevelPlan);
-console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+console.log(simpleLevel)
+let display = new DOMDisplay(document.body, simpleLevel);
+display.syncState(State.start(simpleLevel));
