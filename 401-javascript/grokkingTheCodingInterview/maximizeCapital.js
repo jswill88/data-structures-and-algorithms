906b33d3d541e0367e27b36faf5add59cb69774b
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-03-25 21:11:03 -0700

    complete maximize capital coding challenge

---
+++ 401-javascript/grokkingTheCodingInterview/maximizeCapital.js
@@ -0,0 +1,29 @@
+const Heap = require('./collections/heap');
+
+const find_maximum_capital = function(capital, profits, numberOfProjects, initialCapital) {
+  let totalCapital = initialCapital;
+
+  const profitsMaxHeap = new Heap([], null, (a,b) => a[0] - b[0])
+  const capitalMinHeap = new Heap([], null, (a,b) => b[0] - a[0])
+
+  for(let i = 0; i < capital.length; i++) {
+    capitalMinHeap.push([capital[i], i])
+  }
+
+  for (let j = 0; j < numberOfProjects; j++) {
+    while(capitalMinHeap.peek() && capitalMinHeap.peek()[0] <= totalCapital) {
+      const [_, k] = capitalMinHeap.pop();
+      profitsMaxHeap.push([profits[k], k]);
+    }
+
+    if(!profitsMaxHeap.peek()) break;
+
+    totalCapital += profitsMaxHeap.pop()[0];
+  }
+  
+  return totalCapital;
+};
+
+
+console.log(`Maximum capital: ${find_maximum_capital([0, 1, 2], [1, 2, 3], 2, 1)}`)
+console.log(`Maximum capital: ${find_maximum_capital([0, 1, 2, 3], [1, 2, 3, 5], 3, 0)}`)
