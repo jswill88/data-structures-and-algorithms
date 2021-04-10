d2e92814f3d5c88c9dd794fd9a57cc999b666f2d
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-04-09 20:48:34 -0700

    Refactor find unique trees challenge

--- 401-javascript/grokkingTheCodingInterview/structurallyUniqueBinarySearchTrees.js
+++ 401-javascript/grokkingTheCodingInterview/structurallyUniqueBinarySearchTrees.js
@@ -5,28 +5,26 @@ class TreeNode {
     this.right = right;
   }
 }
-
-const find_unique_trees = function (n) {
-  const makeTree = (min, max) => {
-    if (max < min) return [null];
-    const result = []
-    for (let i = min; i <= max; i++) {
-      const left = makeTree(min, i - 1);
-      const right = makeTree(i + 1, max);
-      for (let l = 0; l < left.length; l++) {
-        for (let r = 0; r < right.length; r++) {
-          const root = new TreeNode(i);
-          root.left = left[l];
-          root.right = right[r];
-          result.push(root)
-        }
+const makeTree = (min, max) => {
+  if (max < min) return [null];
+  const result = []
+  for (let i = min; i <= max; i++) {
+    const left = makeTree(min, i - 1);
+    const right = makeTree(i + 1, max);
+    for (let l = 0; l < left.length; l++) {
+      for (let r = 0; r < right.length; r++) {
+        const root = new TreeNode(i, left[l], right[r]);
+        result.push(root)
       }
     }
-    return result;
   }
+  return result;
+}
+
+const find_unique_trees = function (n) {
+  if (n <= 1) return n;
   return makeTree(1, n)
 };
-
 
 console.log(`Total trees:`, find_unique_trees(2))
 console.log(`Total trees:`, find_unique_trees(4));
